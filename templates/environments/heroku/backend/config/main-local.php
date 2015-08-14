de98334895d19195c3469b863f43acc9092790e6
Kieren Eaton <circledev@gmail.com>
2015-08-14 20:28:15 +0800

    Updated backend config

--- templates/environments/heroku/backend/config/main-local.php
+++ templates/environments/heroku/backend/config/main-local.php
@@ -1,18 +1,13 @@
 <?php
-return [
+
+$config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => '',
-            // unique CSRF cookie parameter for admin (set by kartik-v/yii2-app-practical)
-            'csrfParam' => '_adminCsrf',
+            'csrfParam' => '_backendCSRF',
         ],
-        // unique identity cookie parameter for admin (set by kartik-v/yii2-app-practical)
-        'user' => [
-            'identityCookie' => [
-                'name' => '_adminUser', // unique for admin
-                'path' => '/admin' // set it to correct path for admin app.
-            ]
-        ]
     ],
 ];
+
+return $config;
